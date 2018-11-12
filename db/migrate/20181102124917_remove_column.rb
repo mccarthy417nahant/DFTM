6121131d33073dae2eb6154ea5f171495c38e3cd
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-11-12 16:17:13 -0500

    carrierwave

--- db/migrate/20181102124917_remove_column.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumn < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :ingredients, :quantity
-  end
-
-  def down
-    add_column :ingredients, :quantity, :float
-  end 
-end
